033f412fcdd9222078f86656ca66e4c4c1b7492c
Месье Владос <51823177+Katanych@users.noreply.github.com>
2023-04-20 17:52:49 +0300

    add remove icon
    using method setParent(None)

--- plugin.py
+++ plugin.py
@@ -17,12 +17,9 @@ class DigitizrPlugin(object):
 
     def tr(self, message):
         """Get the translation for a string using Qt translation API.
-
         We implement this ourselves since we do not inherit QObject.
-
         :param message: String for translation.
         :type message: str, QString
-
         :returns: Translated version of message.
         :rtype: QString
         """
@@ -70,6 +67,7 @@ class DigitizrPlugin(object):
         self.toolAddLineBufferButton.setDefaultAction(self.actionAddLineBuffer)
 
     def removeToolAddLineBufferButton(self):
+        self.toolAddLineBufferButton.setParent(None) 
         self._iface.removeToolBarIcon(self.actionAddLineBuffer)
 
     def showToolAddLineBufferButtonSettings(self):
